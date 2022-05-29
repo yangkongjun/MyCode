76411b5afcdcf3f63a74b449357846b565aed6d9
yangkongjun <87958320+yangkongjun@users.noreply.github.com>
2022-05-29 20:31:06 +0800

    Update runing.java

--- runing.java
+++ runing.java
@@ -1,30 +1,27 @@
 package p03;
 
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.net.URL;
 
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 import javax.swing.SwingConstants;
+
 
 /*
  * 3个空行：基本模块相互之间各不搭边
  * 2个空行：程序一个模块的子小功能
  * 1个空行：又并列情况
- *我来测试一下可不可以用了。
  * */
 
 public class runing {
 
-
 	public static void main(String[] args) {
 		
-		while(true) {												//一直循环知道结束====第一层循环。
+		while(true) {												//第一层循环。---实现选择界面和主界面的来回切。
 			GameSelect gs=new GameSelect();
 			gs.setVisible(true);
 			
@@ -68,34 +65,21 @@ public class runing {
 					boolean flag=false;//检测鼠标位置是否在里面
 					@Override
 
-//					public void mousePressed(MouseEvent e) {//实现按下效果。---还可以去实现鼠标经过效果+加线程去显示[线程类]。---好像有问题比如我不左击，我右击。
-//						tool.MOUSE_X=e.getX();
-//						tool.MOUSE_Y=e.getY();
-//						tool.LEFT=true;
-//						//注意：在笛卡尔坐标系中我的y增加。相当于我的x增加。所以一下设置要反一下。
-//						tmpx=(tool.MOUSE_Y-tool.BORDER*3-60)/tool.IN_LEN+1;//60是菜单栏高度
-//						tmpy=(tool.MOUSE_X-tool.BORDER-10)/tool.IN_LEN+1;//误差10应该是窗口边框占10
-//						System.out.println(tmpx+","+tmpy);
-//						if(flag)ga.lb2[tmpx][tmpy].setIcon(new ImageIcon(MapTop.class.getResource("112.png")));
-//					}
-					
+
 					public void mouseReleased(MouseEvent e) {
 
 						if(tool.STATE==-1)return;//													游戏结束，无法继续鼠标点击，只能重新开始游戏。
 						
-						
-						
 			//================解决点击按钮以外也会把首行首列展示的情况。--实现：判断是否鼠标位置在我们的偏移量里面。======
 						
-						flag=false;
+						flag=false;//定义是否超出条件。
 						tool.MOUSE_X=e.getX();
 						tool.MOUSE_Y=e.getY();
 						//																			注意：在笛卡尔坐标系中我的y增加。相当于我的x增加。所以一下设置要反一下。
 						tmpx=(tool.MOUSE_Y-tool.BORDER*3-60)/tool.IN_LEN+1;//						60是菜单栏高度
 						tmpy=(tool.MOUSE_X-tool.BORDER-10)/tool.IN_LEN+1;//							误差10应该是窗口边框占10
-						tool.LEFT=true;
 						
-//						System.out.println(tmpx+","+tmpy);
+						
 						if(tool.MOUSE_X>tool.BORDER+10&&tool.MOUSE_Y>tool.BORDER*3+60) flag=true;
 						if(!flag) return;//															不符合条件直接退出
 						
@@ -105,33 +89,34 @@ public class runing {
 						
 						
 						if(e.getButton()==1) {//													左键按下
-							
+							tool.LEFT=true;
 							if(tmpx>=1&&tmpx<=tool.MAP_H &&tmpy>=1&&tmpy<=tool.MAP_W) {
 								if(tool.MAP_TOP[tmpx][tmpy]==0) {
 									ga.show(tmpx,tmpy);
-//									System.out.println(tool.TOTAL);
-									//可以整合成一个失败函数
 									if(ga.Check()) {//是否踩到地雷？
 										ga.allboom();
 										JOptionPane.showMessageDialog(null, "你踩雷了！");
-										ga.btn1.setIcon(new ImageIcon(Game.class.getResource("bad.png")));
+										URL url = getClass().getResource("../image/bad.png");
+										Icon icon = new ImageIcon(url);
+										ga.btn1.setIcon(icon);
+//										ga.btn1.setIcon(new ImageIcon(Game.class.getResource("bad.png")));
 										tool.STATE=-1;
 									}
 									
 									else if(tool.TOTAL==tool.BOOM){//
 										ga.allopen();
 										JOptionPane.showMessageDialog(null, "你成功了！");
-										ga.btn1.setIcon(new ImageIcon(Game.class.getResource("happy.png")));
+										URL url = getClass().getResource("../image/happy.png");
+										Icon icon = new ImageIcon(url);
+										ga.btn1.setIcon(icon);
+//										ga.btn1.setIcon(new ImageIcon(Game.class.getResource("happy.png")));
 										tool.STATE=-1;
 									}
 									//可以整合成一个成功函数
-									
 								}
-														
 							}
 							
 							tool.LEFT=false;
-//							System.out.println(e.getX()+","+e.getY()); 					
 						}
 						else if(e.getButton()==2) {//												中键按下
 							tool.MOUSE_X=e.getX();
@@ -141,28 +126,38 @@ public class runing {
 						}
 						else if(e.getButton()==3) {//												右键按下
 							tool.RIGHT=true;
-//							System.out.println(e.getX()+","+e.getY());
-							
 							if(tool.MAP_TOP[tmpx][tmpy]==0) {
 								tool.MAP_TOP[tmpx][tmpy]=1;
-								ga.lb2[tmpx][tmpy].setIcon(new ImageIcon(GameSelect.class.getResource("TopFlag.png")));
+								URL url = getClass().getResource("../image/TopFlag.png");
+								Icon icon = new ImageIcon(url);
+								ga.lb2[tmpx][tmpy].setIcon(icon);
+//								ga.lb2[tmpx][tmpy].setIcon(new ImageIcon(GameSelect.class.getResource("TopFlag.png")));
 							}
 							else if(tool.MAP_TOP[tmpx][tmpy]==1) {
 								tool.MAP_TOP[tmpx][tmpy]=0;
-								ga.lb2[tmpx][tmpy].setIcon(new ImageIcon(GameSelect.class.getResource("11.png")));
+								URL url = getClass().getResource("../image/11.png");
+								Icon icon = new ImageIcon(url);
+								ga.lb2[tmpx][tmpy].setIcon(icon);
+//								ga.lb2[tmpx][tmpy].setIcon(new ImageIcon(GameSelect.class.getResource("11.png")));
 							}
 							else if(tool.MAP_TOP[tmpx][tmpy]==-1) {
 								ga.look(tmpx,tmpy);
 								if(ga.Check()) {
 									ga.allboom();
 									JOptionPane.showMessageDialog(null, "你踩雷了！");
-									ga.btn1.setIcon(new ImageIcon(Game.class.getResource("bad.png")));
+									URL url = getClass().getResource("../image/bad.png");
+									Icon icon = new ImageIcon(url);
+									ga.btn1.setIcon(icon);
+//									ga.btn1.setIcon(new ImageIcon(Game.class.getResource("bad.png")));
 									tool.STATE=-1;
 								}
 								if(tool.TOTAL==tool.BOOM){
 									ga.allopen();
 									JOptionPane.showMessageDialog(null, "你成功了！");
-									ga.btn1.setIcon(new ImageIcon(Game.class.getResource("happy.png")));
+									URL url = getClass().getResource("../image/happy.png");
+									Icon icon = new ImageIcon(url);
+									ga.btn1.setIcon(icon);
+//									ga.btn1.setIcon(new ImageIcon(Game.class.getResource("happy.png")));
 									tool.STATE=-1;
 								}
 							}
@@ -173,7 +168,7 @@ public class runing {
 				});
 				//如果监控放下面，那就一直先执行while死循环，监听代码都没运行
 				//************游戏界面的死循环运行---鼠标监听在while循环外************
-				int time=0;//因为无法实现在while中即实现1秒增加1.又要实现0.05秒检测一下是否退出。所以用了这个办法来解决。
+				int time=1000;//因为无法实现在while中即实现1秒增加1.又要实现0.05秒检测一下是否退出。所以用了这个办法来解决。
 				while(true) {											//退出游戏后进入选择界面
 					
 					try {
@@ -197,10 +192,8 @@ public class runing {
 						e1.printStackTrace();
 					}//用来检测是否要重新设置游戏。
 				}
+				
 			}
-			
-
-			
 			
 
 		}
